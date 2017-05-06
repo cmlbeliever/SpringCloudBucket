db94f7f1365196fcd697b0045bf5b0a157d8c7c8
admin <865517964@qq.com>
2017-05-06 11:06:36 +0800

    server client init

--- SpringCloudTest/src/main/java/com/cml/springcloud/Application.java
+++ SpringCloudTest/src/main/java/com/cml/springcloud/Application.java
@@ -3,9 +3,11 @@ package com.cml.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableConfigServer
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
