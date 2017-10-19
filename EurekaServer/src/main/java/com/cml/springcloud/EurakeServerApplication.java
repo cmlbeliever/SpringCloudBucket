741ebcac1a980fef42577441efaa59fbe81af9dc
cml <menglin.chen>
2017-10-19 10:10:33 +0800

    add eureka server

---
+++ EurekaServer/src/main/java/com/cml/springcloud/EurakeServerApplication.java
@@ -0,0 +1,13 @@
+package com.cml.springcloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurakeServerApplication {
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(EurakeServerApplication.class).web(true).run(args);
+	}
+}
