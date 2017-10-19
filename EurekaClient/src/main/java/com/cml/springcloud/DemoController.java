1c89ec7f75c3e7e17fa0c2b3e1c7d6caaf712a26
cml <menglin.chen>
2017-10-19 17:49:58 +0800

    客户端发现代码添加

--- EurekaClient/src/main/java/com/cml/springcloud/DemoController.java
+++ EurekaClient/src/main/java/com/cml/springcloud/DemoController.java
@@ -2,8 +2,25 @@ package com.cml.springcloud;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
+@RequestMapping("/")
 public class DemoController {
 	@Autowired
 	private DiscoveryClient client;
+	
+	@ResponseBody
+	@RequestMapping("/info")
+	public Object info() {
+		return client.getServices();
+	}
+	
+	@ResponseBody
+	@RequestMapping("/info2")
+	public Object info2() {
+		return client.getServices();
+	}
 }
