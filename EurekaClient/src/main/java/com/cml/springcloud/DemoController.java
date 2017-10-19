8ac897c35cb20a76532ddfaaf5f2611fa6c2d73f
admin <865517964@qq.com>
2017-10-19 21:22:13 +0800

    update

--- EurekaClient/src/main/java/com/cml/springcloud/DemoController.java
+++ EurekaClient/src/main/java/com/cml/springcloud/DemoController.java
@@ -1,6 +1,7 @@
 package com.cml.springcloud;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,18 +10,29 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/")
 public class DemoController {
+
+	@Value("spring.application.name")
+	private String port;
+	@Value("server.port")
+	private String sererName;
+
 	@Autowired
 	private DiscoveryClient client;
-	
+
 	@ResponseBody
 	@RequestMapping("/info")
 	public Object info() {
 		return client.getServices();
 	}
-	
+
 	@ResponseBody
 	@RequestMapping("/info2")
 	public Object info2() {
 		return client.getServices();
+	}
+
+	@RequestMapping("/hello")
+	public String sayHello(String req) {
+		return "server:" + req;
 	}
 }
