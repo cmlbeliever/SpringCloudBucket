29e4d9175adf8b0693e470196ee0f46fb65f3e3b
cml <menglin.chen>
2017-10-20 12:27:12 +0800

    去除配置中心

--- EurekaServer/src/main/java/com/cml/springcloud/EurakeServerApplication.java
+++ EurekaServer/src/main/java/com/cml/springcloud/EurakeServerApplication.java
@@ -2,10 +2,8 @@ package com.cml.springcloud;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableConfigServer
 @EnableEurekaServer
 @SpringBootApplication
 public class EurakeServerApplication {
