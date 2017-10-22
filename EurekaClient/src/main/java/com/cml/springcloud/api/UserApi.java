ade5bb58b2b7e8df574b090a4be7ae139ad2cdb9
admin <865517964@qq.com>
2017-10-22 14:33:07 +0800

    zuul 配置

--- EurekaClient/src/main/java/com/cml/springcloud/api/UserApi.java
+++ EurekaClient/src/main/java/com/cml/springcloud/api/UserApi.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("EUREKA-USER")
+@FeignClient("APPLICATION-USER")
 public interface UserApi {
 
 	@RequestMapping(value = "/getUser")
