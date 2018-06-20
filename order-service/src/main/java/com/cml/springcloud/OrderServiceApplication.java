02cd3788fcde0555071086de1e2f3ad0e4a839a7
chenmenglin <you@example.com>
2018-06-21 00:03:58 +0800

    监控功能添加

--- order-service/src/main/java/com/cml/springcloud/OrderServiceApplication.java
+++ order-service/src/main/java/com/cml/springcloud/OrderServiceApplication.java
@@ -2,14 +2,25 @@ package com.cml.springcloud;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableFeignClients
-@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 public class OrderServiceApplication {
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(OrderServiceApplication.class).web(true).run(args);
+	}
+	@Configuration
+	public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests().anyRequest().permitAll()
+					.and().csrf().disable();
+		}
 	}
 }
