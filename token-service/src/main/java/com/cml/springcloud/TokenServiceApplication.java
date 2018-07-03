0a696aed2599dc4629e9482b12940ac9488111c4
chenmenglin <you@example.com>
2018-07-03 16:56:18 +0800

    token 工程添加到admin

--- token-service/src/main/java/com/cml/springcloud/TokenServiceApplication.java
+++ token-service/src/main/java/com/cml/springcloud/TokenServiceApplication.java
@@ -3,14 +3,23 @@ package com.cml.springcloud;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@EnableScheduling
+//@EnableScheduling
 @EnableDiscoveryClient
 @SpringBootApplication
 public class TokenServiceApplication {
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(TokenServiceApplication.class).web(true).run(args);
+	}
+	@Configuration
+	public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests().anyRequest().permitAll()
+					.and().csrf().disable();
+		}
 	}
 }
