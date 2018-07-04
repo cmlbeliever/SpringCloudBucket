866384bc4a8676737f31b75d1aed003404a27ae2
chenmenglin <you@example.com>
2018-07-04 10:41:43 +0800

    auth-service 添加到admin

--- auth-service/src/main/java/com/cml/springcloud/AuthCenterApplication.java
+++ auth-service/src/main/java/com/cml/springcloud/AuthCenterApplication.java
@@ -6,13 +6,24 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableEurekaClient
 @EnableDiscoveryClient
 @SpringBootApplication
 public class AuthCenterApplication {
-	public static void main(String[] args) {
-		new SpringApplicationBuilder(AuthCenterApplication.class).web(true).run(args);
-	}
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(AuthCenterApplication.class).web(true).run(args);
+    }
 
+    @Configuration
+    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests().anyRequest().permitAll()
+                    .and().csrf().disable();
+        }
+    }
 }
