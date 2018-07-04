1c6f5ebd635cf5407d612584bda7313f5464919e
chenmenglin <you@example.com>
2018-07-04 11:01:40 +0800

    api-getway 添加到admin

--- api-getway/src/main/java/com/cml/springcloud/ZuulClientApplication.java
+++ api-getway/src/main/java/com/cml/springcloud/ZuulClientApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableRetry
 @EnableZuulProxy
@@ -16,11 +18,18 @@ import org.springframework.retry.annotation.EnableRetry;
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableHystrix
-@EnableHystrixDashboard
+//@EnableHystrixDashboard
 @EnableCircuitBreaker
 public class ZuulClientApplication {
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(ZuulClientApplication.class).web(true).run(args);
 	}
-
+	@Configuration
+	public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests().anyRequest().permitAll()
+					.and().csrf().disable();
+		}
+	}
 }
--- api-getway/src/main/java/com/cml/springcloud/api/UserApi.java
+++ api-getway/src/main/java/com/cml/springcloud/api/UserApi.java
@@ -1,7 +1,7 @@
 package com.cml.springcloud.api;
 
 import com.cml.springcloud.api.fallback.UserApiFallback;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
