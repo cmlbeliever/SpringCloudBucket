954ed62dae930e8384ed875d58e45b494d129fa8
chenmenglin <you@example.com>
2018-07-26 19:13:47 +0800

    添加自定义监控

--- user-service/src/main/java/com/cml/springcloud/UserClientApplication.java
+++ user-service/src/main/java/com/cml/springcloud/UserClientApplication.java
@@ -1,7 +1,9 @@
 package com.cml.springcloud;
 
+import io.micrometer.core.instrument.Metrics;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.actuate.autoconfigure.jdbc.DataSourceHealthIndicatorAutoConfiguration;
 import org.springframework.boot.actuate.autoconfigure.metrics.jdbc.DataSourcePoolMetricsAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +11,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -19,12 +22,31 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class UserClientApplication {
     static Logger logger = LoggerFactory.getLogger(UserClientApplication.class);
 
+    private static final String CUSTOM_COUNTER = "xxl_my_counter";
+
     public static void main(String[] args) {
         try {
             new SpringApplicationBuilder(UserClientApplication.class).web(true).run(args);
         } catch (Exception e) {
             logger.error("", e);
         }
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner() {
+        return (arg) -> {
+            new Thread(() -> {
+                for (int i = 0; i < 10000; i++) {
+                    Metrics.counter(CUSTOM_COUNTER, "test", "失败的次数").increment((int) (100 * Math.random()));
+                    Metrics.counter(CUSTOM_COUNTER, "test2", "成功的次数").increment((int) (100 * Math.random()));
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        };
     }
 
     @Configuration
