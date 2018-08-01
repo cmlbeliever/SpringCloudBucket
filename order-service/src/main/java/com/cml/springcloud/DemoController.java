00dc621edb60a8482ddcc4e40b879a4b0c0329ea
chenmenglin <you@example.com>
2018-08-01 11:05:31 +0800

    添加优雅下线测试

--- order-service/src/main/java/com/cml/springcloud/DemoController.java
+++ order-service/src/main/java/com/cml/springcloud/DemoController.java
@@ -5,12 +5,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EurekaDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.serviceregistry.EurekaAutoServiceRegistration;
+import org.springframework.cloud.netflix.eureka.serviceregistry.EurekaServiceRegistry;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.cml.springcloud.api.UserApi;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 @RefreshScope
 @Controller
@@ -34,6 +40,9 @@ public class DemoController {
 
     @Autowired
     private UserApi userApi;
+
+    @Autowired
+    private EurekaAutoServiceRegistration eurekaAutoServiceRegistration;
 
     @ResponseBody
     @RequestMapping("/test")
