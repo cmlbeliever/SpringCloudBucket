01a66b7d60c90c065137e49447bf1f7025aea142
admin <865517964@qq.com>
2019-01-30 22:15:31 +0800

    springcloud gateway添加

---
+++ springcloud-gateway/src/main/java/com/cml/learn/springcloud/gateway/SpringCloudGatewayApplication.java
@@ -0,0 +1,11 @@
+package com.cml.learn.springcloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudGatewayApplication.class, args);
+    }
+}
